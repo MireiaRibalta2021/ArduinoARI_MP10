a456c056e542ef631f52ef73ffadbb6ec5a8df14
MireiaRibalta2021 <mireia.ribalta.2039@lacetania.cat>
2021-04-12 12:50:32 +0200

    Create

--- Cruïlla de semàfors/001_Cruilla_Semafors.cpp/001_Cruilla_Semafors.cpp.ino
+++ Cruïlla de semàfors/001_Cruilla_Semafors.cpp/001_Cruilla_Semafors.cpp.ino
@@ -1,100 +1,79 @@
 /**********************************************************************************
 **                                                                               **
-**                              Cruïlla de semàfors                              **
+**                            Display de 7 segments                              **
 **                                                                               **
 **                                                                               **
 **********************************************************************************/
+
 //********** Includes *************************************************************
+
 //********** Variables ************************************************************
-const int ledr1 = 7;          // donar nom al pin 7 de l’Arduino
-const int ledg1 = 8;          // donar nom al pin 8 de l’Arduino
-const int ledv1 = 9;          // donar nom al pin 9 de l’Arduino
-const int ledr2 = 10;         // donar nom al pin 10 de l’Arduino
-const int ledg2 = 11;         // donar nom al pin 11 de l’Arduino
-const int ledv2 = 12;         // donar nom al pin 12 de l’Arduino
+const int leda = 5;          // donar nom al pin 5 de l’Arduino
+const int ledb = 6;          // donar nom al pin 6 de l’Arduino
+const int ledc = 7;          // donar nom al pin 7 de l’Arduino
+const int ledd = 8;          // donar nom al pin 8 de l’Arduino
+const int lede = 9;          // donar nom al pin 9 de l’Arduino
+const int ledf = 10;         // donar nom al pin 10 de l’Arduino
+const int ledg = 11;         // donar nom al pin 11 de l’Arduino
+
 
 //********** Setup ****************************************************************
 void setup()
 {
- 
-  pinMode(ledr1, OUTPUT);     // definir el pin 7 com una sortida
-  pinMode(ledg1, OUTPUT);     // definir el pin 8 com una sortida
-  pinMode(ledv1, OUTPUT);     // definir el pin 9 com una sortida
-  pinMode(ledr2, OUTPUT);     // definir el pin 10 com una sortida
-  pinMode(ledg2, OUTPUT);     // definir el pin 11 com una sortida
-  pinMode(ledv2, OUTPUT);     // definir el pin 12 com una sortida
+  pinMode(leda, OUTPUT);     // definir el pin 5 com una sortida
+  pinMode(ledb, OUTPUT);     // definir el pin 6 com una sortida
+  pinMode(ledc, OUTPUT);     // definir el pin 7 com una sortida
+  pinMode(ledd, OUTPUT);     // definir el pin 8 com una sortida
+  pinMode(lede, OUTPUT);     // definir el pin 9 com una sortida
+  pinMode(ledf, OUTPUT);     // definir el pin 10 com una sortida
+  pinMode(ledg, OUTPUT);     // definir el pin 11 com una sortida
 }
+
 //********** Loop *****************************************************************
 void loop()
 {
-  digitalWrite(ledr1, HIGH);        //leds vermells encesos 
-  digitalWrite(ledr2, HIGH); 
-  delay (200);  
-  digitalWrite(ledr1, HIGH);        //s'encen el verd 2 i s'apaga el vermell 2
-  digitalWrite(ledv2, HIGH);
-  digitalWrite(ledr2, LOW); 
-  delay (200);  
-  digitalWrite(ledr1, HIGH);
-  digitalWrite(ledv2, HIGH);
-  delay (200); 
-  digitalWrite(ledr1, HIGH);
-  digitalWrite(ledv2, HIGH);
-  delay (200); 
-  digitalWrite(ledr1, HIGH);
-  digitalWrite(ledv2, HIGH);
-  delay (200); 
-  digitalWrite(ledr1, HIGH);         //s'apaga led verd2 i comença intermitencia led groc
-  digitalWrite(ledg2, HIGH);
-  digitalWrite(ledv2, LOW); 
-  delay (200); 
-  digitalWrite(ledr1, HIGH);
-  digitalWrite(ledg2, LOW);
-  delay (200); 
-  digitalWrite(ledr1, HIGH);
-  digitalWrite(ledg2, HIGH);
-  delay (200); 
-  digitalWrite(ledr1, HIGH);
-  digitalWrite(ledg2, LOW);
-  delay (200); 
-  digitalWrite(ledr1, HIGH);
-  digitalWrite(ledg2, HIGH);
-  delay (200); 
-  digitalWrite(ledr1, HIGH);         //leds vermells encesos
-  digitalWrite(ledg2, LOW);
-  digitalWrite(ledr2, HIGH);
-  delay (200); 
-  digitalWrite(ledv1, HIGH);        //led verd 1 s'encen i led vermell 1 s'apaga
-  digitalWrite(ledr1, LOW);        
-  digitalWrite(ledr2, HIGH);
-  delay(200);
-  digitalWrite(ledv1, HIGH);               
-  digitalWrite(ledr2, HIGH);
-  delay(200);
-  digitalWrite(ledv1, HIGH);               
-  digitalWrite(ledr2, HIGH);
-  delay(200);
-  digitalWrite(ledv1, HIGH);               
-  digitalWrite(ledr2, HIGH);
-  delay(200);
-  digitalWrite(ledg1, HIGH);        //led verd 1 s'apaga comença intermitencia led groc 1
-  digitalWrite(ledv1, LOW);               
-  digitalWrite(ledr2, HIGH);
-  delay(200);
-  digitalWrite(ledg1, LOW);               
-  digitalWrite(ledr2, HIGH);
-  delay(200);
-  digitalWrite(ledv1, HIGH);               
-  digitalWrite(ledr2, HIGH);
-  delay(200);
-  digitalWrite(ledg1, LOW);               
-  digitalWrite(ledr2, HIGH);
-  delay(200);
-  digitalWrite(ledg1, HIGH);               
-  digitalWrite(ledr2, HIGH);
-  delay(200);
-  digitalWrite(ledg1, LOW);         //torna a vermell tots dos semafors.
-  digitalWrite(ledr1, HIGH);               
-  digitalWrite(ledr2, HIGH);
-  delay(200);
- }
+  digitalWrite(ledd, HIGH);    //M
+  digitalWrite(ledb, HIGH);    
+  digitalWrite(ledf, HIGH);    
+  delay(500);                  // es queden leds 500ms encesos
+  
+  digitalWrite(ledf, LOW);     //I
+  digitalWrite(lede, HIGH);     
+  digitalWrite(ledb, LOW);     
+  digitalWrite(ledd, HIGH);     
+  
+  delay(500);                  // es queden leds 500ms apagats
+  digitalWrite(lede, LOW);     //R
+  digitalWrite(ledd, HIGH);     
+  digitalWrite(ledg, HIGH);        
+  
+  delay(500);                  // es queden leds 500ms apagats
+  digitalWrite(ledf, HIGH);     //E
+  digitalWrite(lede, HIGH);     
+  digitalWrite(ledg, HIGH);     
+  digitalWrite(ledd, HIGH);     
+  digitalWrite(ledc, HIGH);
+  delay(500);                  // es queden leds 500ms apagats
+  digitalWrite(ledf, LOW);     //I
+  digitalWrite(lede, HIGH);     
+  digitalWrite(ledg, LOW);     
+  digitalWrite(ledd, HIGH);     
+  digitalWrite(ledc, LOW);
+  delay(500);                  // es queden leds 500ms apagats
+  digitalWrite(ledf, HIGH);     //A
+  digitalWrite(lede, HIGH);     
+  digitalWrite(leda, HIGH);     
+  digitalWrite(ledg, HIGH);     
+  digitalWrite(ledd, HIGH);
+  digitalWrite(ledb, HIGH); 
+  delay(500);                  // es queden leds 500ms apagats
+  digitalWrite(ledf, LOW);     //A
+  digitalWrite(lede, LOW);     
+  digitalWrite(leda, LOW);     
+  digitalWrite(ledg, LOW);     
+  digitalWrite(ledd, LOW);
+  digitalWrite(ledb, LOW); 
+  delay(500);                  // es queden leds 500ms apagats
+}
+
 //********** Funcions *************************************************************
