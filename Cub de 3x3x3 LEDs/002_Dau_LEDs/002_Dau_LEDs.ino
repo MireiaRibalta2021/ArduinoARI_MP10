2f61bc7783c52e572c0ab157b92bbe137d677896
MireiaRibalta2021 <mireia.ribalta.2039@lacetania.cat>
2021-04-13 16:40:55 +0200

    update

--- Cub de 3x3x3 LEDs/002_Dau_LEDs/002_Dau_LEDs.ino
+++ Cub de 3x3x3 LEDs/002_Dau_LEDs/002_Dau_LEDs.ino
@@ -8,94 +8,83 @@
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
+const int temps = 1000;
+const int led4 = 5;          // donar nom al pin 5 de l’Arduino
+const int led3 = 6;          // donar nom al pin 6 de l’Arduino
+const int led2 = 7;          // donar nom al pin 7 de l’Arduino
+const int led1 = 8;          // donar nom al pin 8 de l’Arduino
+int buttonState;
+int buttonPin = 2;
+long aleatori;
 
-const int led4 = 5;
-const int led3 = 6;
-const int led2 = 7;
-const int led1 = 8;
-const int buttonPin = 2; 
-int buttonState = 0;     
-int Comptador = 0;
-int temps = 500;
-
-void setup() 
+//********** Setup ****************************************************************
+void setup()
 {
-   pinMode(led4, OUTPUT);
-   pinMode(led3, OUTPUT);
-   pinMode(led2, OUTPUT);
-   pinMode(led1, OUTPUT);
-   pinMode(buttonPin, INPUT); 
+  
+  pinMode(led1, OUTPUT);     
+  pinMode(led2, OUTPUT);     
+  pinMode(led3, OUTPUT);    
+  pinMode(led4, OUTPUT); 
+  pinMode(buttonPin, INPUT);
 }
 
-void loop() 
+//********** Loop *****************************************************************
+void loop()
 {
   buttonState = digitalRead(buttonPin);
-  if (buttonState == 0)
+  
+  
+  if (buttonState == 0)  
   {
-    
-  Comptador = Comptador++;
-    
-    if (buttonPushCounter == 1)
-   { 
+    aleatori = random(1, 6);
+    delay(500);
+  }
+  switch (aleatori)
+  {
+    case 1:
      digitalWrite(led4, LOW);       // 1
      digitalWrite(led3, LOW);
      digitalWrite(led2, LOW);
      digitalWrite(led1, HIGH); 
      delay(temps);
-    }
-    else if (Comptador == 2)
-  { 
+    break;
+    case 2:
      digitalWrite(led4, HIGH);      // 2
      digitalWrite(led3, LOW);
      digitalWrite(led2, LOW);
      digitalWrite(led1, LOW);
      delay(temps);
-   }
-    else if (Comptador == 3) 
-  {
+    break;
+    case 3:
      digitalWrite(led4, LOW);      // 3
      digitalWrite(led3, HIGH);
      digitalWrite(led2, LOW);
      digitalWrite(led1, HIGH);
      delay(temps);
-  } 
-     else if (Comptador == 4)
-  { 
+    break;
+    case 4:
      digitalWrite(led4, LOW);       // 4 
      digitalWrite(led3, HIGH);
      digitalWrite(led2, HIGH);
      digitalWrite(led1, LOW);
      delay(temps);
-  }   
-     else if (Comptador == 5) 
-     {
+    break;
+    case 5:
      digitalWrite(led4, LOW);       // 5
      digitalWrite(led3, HIGH);
      digitalWrite(led2, HIGH);
      digitalWrite(led1, HIGH);
-      
-      delay(temps);   
-  } 
-     else if (Comptador == 6)
-     {
+     delay(temps);
+    break;
+    case 6:
      digitalWrite(led4, HIGH);      // 6
      digitalWrite(led3, HIGH);
      digitalWrite(led2, HIGH);
      digitalWrite(led1, LOW);
      delay(temps);
-   }
-      else if (Comptador == 0)
-   {
-     digitalWrite(led4, LOW);
-     digitalWrite(led3, LOW);
-     digitalWrite(led2, LOW);
-     digitalWrite(led1, LOW);
-     
-      delay(temps); 
-   }
-      else  
-   {
-     Comptador = 0;
-   }
- }
+    default:
+    break;
+  } 
 }
+
+//********** Funcions ************************************************************
